e4fe8f48ddd670095daf782b0f80599b7af2d3fb
Aaron Shim <5382864+aaronshim@users.noreply.github.com>
2022-12-10 02:11:01 +0000

    Day 9 debugged with visualizations.

--- lib/day9/main.ml
+++ lib/day9/main.ml
@@ -61,9 +61,17 @@ let%expect_test "is_tail_ok" =
 
 (** Move either one square in one direction or one square in both directions. *)
 let move_tail (x_h, y_h) (x_t, y_t) =
-  let x_t' = if x_h - x_t > 0 then x_t + 1 else if x_h - x_t < 0 then x_t - 1 else x_t in
-  let y_t' = if y_h - y_t > 0 then y_t + 1 else if y_h - y_t < 0 then y_t - 1 else y_t in
-  x_t', y_t'
+  (* Forcing this check to happen here so that we don't move inadvertently. *)
+  if is_tail_ok (x_h, y_h) (x_t, y_t)
+  then x_t, y_t
+  else (
+    let x_t' =
+      if x_h - x_t > 0 then x_t + 1 else if x_h - x_t < 0 then x_t - 1 else x_t
+    in
+    let y_t' =
+      if y_h - y_t > 0 then y_t + 1 else if y_h - y_t < 0 then y_t - 1 else y_t
+    in
+    x_t', y_t')
 ;;
 
 let%expect_test "move_tail" =
@@ -76,33 +84,32 @@ let%expect_test "move_tail" =
   [%expect {| ((2 1) (1 2) (2 2) (2 2)) |}]
 ;;
 
-let s = Sexp.to_string [%sexp (0, 0 : int * int)]
-
-(** For folding. *)
-let rec f ((x_h, y_h), (x_t, y_t), visited) direction =
-  if is_done_moving_in_direction direction
-  then (x_h, y_h), (x_t, y_t), visited
-  else (
-    let x_h', y_h', direction' =
-      match direction with
-      | Up n -> x_h, y_h + 1, Up (n - 1)
-      | Down n -> x_h, y_h - 1, Down (n - 1)
-      | Left n -> x_h - 1, y_h, Left (n - 1)
-      | Right n -> x_h + 1, y_h, Right (n - 1)
-    in
-    let x_t', y_t', visited' =
-      if is_tail_ok (x_h', y_h') (x_t, y_t)
-      then x_t, y_t, visited
-      else (
-        let x_t'', y_t'' = move_tail (x_h', y_h') (x_t, y_t) in
-        ( x_t''
-        , y_t''
-        , String.Set.add visited (Sexp.to_string [%sexp (x_t'', y_t'' : int * int)]) ))
-    in
-    f ((x_h', y_h'), (x_t', y_t'), visited') direction')
-;;
-
 module Part1 = struct
+  (** For folding. *)
+  let rec f ((x_h, y_h), (x_t, y_t), visited) direction =
+    if is_done_moving_in_direction direction
+    then (x_h, y_h), (x_t, y_t), visited
+    else (
+      let x_h', y_h', direction' =
+        match direction with
+        | Up n -> x_h, y_h + 1, Up (n - 1)
+        | Down n -> x_h, y_h - 1, Down (n - 1)
+        | Left n -> x_h - 1, y_h, Left (n - 1)
+        | Right n -> x_h + 1, y_h, Right (n - 1)
+      in
+      let x_t', y_t', visited' =
+        (* Doing check here even if it's done in move_tail so that we know whether to update the visited set. *)
+        if is_tail_ok (x_h', y_h') (x_t, y_t)
+        then x_t, y_t, visited
+        else (
+          let x_t'', y_t'' = move_tail (x_h', y_h') (x_t, y_t) in
+          ( x_t''
+          , y_t''
+          , String.Set.add visited (Sexp.to_string [%sexp (x_t'', y_t'' : int * int)]) ))
+      in
+      f ((x_h', y_h'), (x_t', y_t'), visited') direction')
+  ;;
+
   let solve' input =
     input
     |> parse
@@ -129,5 +136,189 @@ module Part1 = struct
   let%expect_test "Solve full input" =
     print_s [%sexp (solve full_input : int)];
     [%expect {| 6563 |}]
+  ;;
+end
+
+module Part2 = struct
+  let ten_on_origin = List.init 10 ~f:(fun _ -> 0, 0)
+  let set_with_origin = String.Set.of_list [ "(0 0)" ]
+
+  let rec visualize' (coords : (int * int) list) (i : int) grid =
+    match coords with
+    | (x, y) :: rest ->
+      (* let _ = print_endline (Int.to_string x) in *)
+      (* let _ = print_endline (Int.to_string y) in *)
+      (* let _ = print_s [%sexp (grid.(y) : string array)] in *)
+      let max_y = Array.length grid in
+      grid.(max_y - y - 1).(x) <- Int.to_string i;
+      visualize' rest (i + 1) grid
+    | [] ->
+      grid |> Array.map ~f:(String.concat_array ~sep:" ") |> String.concat_array ~sep:"\n"
+  ;;
+
+  let visualize coords =
+    let max_x =
+      Option.value_exn
+        (List.max_elt (List.map ~f:Tuple2.get1 coords) ~compare:Int.compare)
+    in
+    let max_y =
+      Option.value_exn
+        (List.max_elt (List.map ~f:Tuple2.get2 coords) ~compare:Int.compare)
+    in
+    (* let _ = print_endline (Int.to_string max_x) in *)
+    (* let _ = print_endline (Int.to_string max_y) in *)
+    let grid =
+      Array.of_list
+        (List.init
+           ((max_y + 1) * 2)
+           ~f:(fun _ -> Array.init ((max_x + 1) * 2) ~f:(fun _ -> "*")))
+    in
+    (* print_s [%sexp (grid : string array array)]; *)
+    visualize' coords 0 grid
+  ;;
+
+  let%expect_test "Visualize" =
+    print_string (visualize [ 0, 0; 0, 1; 2, 3 ]);
+    [%expect
+      {|
+    * * * * * *
+    * * * * * *
+    * * * * * *
+    * * * * * *
+    * * 2 * * *
+    * * * * * *
+    1 * * * * *
+    0 * * * * * |}]
+  ;;
+
+  (** Update all the old coords into new coords by following the last updated one, but in reverse order. *)
+  let rec follow_the_leader
+    (new_coords : (int * int) list)
+    (old_coords : (int * int) list)
+    : (int * int) list
+    =
+    match List.hd old_coords with
+    | None -> new_coords
+    | Some next_coord_to_update ->
+      let last_updated_coord = List.hd_exn new_coords in
+      let just_updated = move_tail last_updated_coord next_coord_to_update in
+      (* let _ = print_endline "Updating..." in
+      let _ = print_s [%sexp (next_coord_to_update : int * int)] in
+      let _ = print_endline "To follow..." in
+      let _ = print_s [%sexp (last_updated_coord : int * int)] in
+      let _ = print_endline "Result..." in
+      let _ = print_s [%sexp (just_updated : int * int)] in *)
+      follow_the_leader (just_updated :: new_coords) (List.tl_exn old_coords)
+  ;;
+
+  (** For folding. *)
+  let rec f (coords_list, visited) direction =
+    if is_done_moving_in_direction direction
+    then (* let _ = print_endline (visualize coords_list) in *)
+      coords_list, visited
+    else (
+      let x_h, y_h = List.hd_exn coords_list in
+      let x_h', y_h', direction' =
+        match direction with
+        | Up n -> x_h, y_h + 1, Up (n - 1)
+        | Down n -> x_h, y_h - 1, Down (n - 1)
+        | Left n -> x_h - 1, y_h, Left (n - 1)
+        | Right n -> x_h + 1, y_h, Right (n - 1)
+      in
+      let new_coords_reversed =
+        follow_the_leader [ x_h', y_h' ] (List.tl_exn coords_list)
+      in
+      let x_t', y_t' = List.hd_exn new_coords_reversed in
+      let visited' =
+        String.Set.add visited (Sexp.to_string [%sexp (x_t', y_t' : int * int)])
+      in
+      let new_coords = List.rev new_coords_reversed in
+      f (new_coords, visited') direction')
+  ;;
+
+  let%expect_test "Visualize (intermediate)" =
+    let coords, visited = f (ten_on_origin, set_with_origin) (Right 4) in
+    print_endline "==========";
+    print_s [%sexp (Right 4 : direction)];
+    print_endline "==========";
+    print_s [%sexp (visited : String.Set.t)];
+    print_endline "==========";
+    print_string (visualize coords);
+    print_endline "";
+    let coords2, visited2 = f (coords, visited) (Up 4) in
+    print_endline "==========";
+    print_s [%sexp (Up 4 : direction)];
+    print_endline "==========";
+    print_s [%sexp (visited2 : String.Set.t)];
+    print_endline "==========";
+    print_string (visualize coords2);
+    let coords3, visited3 = f (coords2, visited2) (Left 3) in
+    print_endline "==========";
+    print_s [%sexp (Left 3 : direction)];
+    print_endline "==========";
+    print_s [%sexp (visited3 : String.Set.t)];
+    print_endline "==========";
+    print_string (visualize coords3);
+    [%expect
+      {|
+    ==========
+    (Right 4)
+    ==========
+    ("(0 0)")
+    ==========
+    * * * * * * * * * *
+    9 3 2 1 0 * * * * *
+    ==========
+    (Up 4)
+    ==========
+    ("(0 0)")
+    ==========
+    * * * * * * * * * *
+    * * * * * * * * * *
+    * * * * * * * * * *
+    * * * * * * * * * *
+    * * * * * * * * * *
+    * * * * 0 * * * * *
+    * * * * 1 * * * * *
+    * * 4 3 2 * * * * *
+    * 5 * * * * * * * *
+    9 * * * * * * * * *==========
+    (Left 3)
+    ==========
+    ("(0 0)")
+    ==========
+    * * * * * * * *
+    * * * * * * * *
+    * * * * * * * *
+    * * * * * * * *
+    * * * * * * * *
+    * 0 1 * * * * *
+    * * * 2 * * * *
+    * * 4 3 * * * *
+    * 5 * * * * * *
+    9 * * * * * * * |}]
+  ;;
+
+  let solve' input =
+    input |> parse |> List.fold ~init:(ten_on_origin, set_with_origin) ~f |> Tuple2.get2
+  ;;
+
+  let solve input = input |> solve' |> Set.length
+
+  let%expect_test "Solve small input" =
+    print_s [%sexp (solve' small_input : String.Set.t)];
+    [%expect {|
+      ("(0 0)") |}]
+  ;;
+
+  let%expect_test "Solve small input" =
+    print_s [%sexp (solve small_input : int)];
+    [%expect {|
+      1 |}]
+  ;;
+
+  let%expect_test "Solve full input" =
+    print_s [%sexp (solve full_input : int)];
+    [%expect {| 2653 |}]
   ;;
 end
