c35de0a974f8e4dad0b0876f040e48482f8a7afe
Aaron Shim <5382864+aaronshim@users.noreply.github.com>
2022-12-11 14:49:31 +0000

    Get rid of BigInt and actually do the mod.

--- lib/day11/main.ml
+++ lib/day11/main.ml
@@ -37,7 +37,7 @@ type operation =
   | TimesOld
 [@@deriving sexp]
 
-type monkey = Bigint.t list * operation * int * int * int * int [@@deriving sexp]
+type monkey = int list * operation * int * int * int * int [@@deriving sexp]
 
 let full_input = Stdio.In_channel.read_all "/workspace/advent2022/data/day11.txt"
 
@@ -58,7 +58,7 @@ let parse_op line =
 
 let parse_monkey lines : monkey =
   let _id = List.nth_exn lines 0 |> Shared.extract_ints |> List.hd_exn in
-  let items = List.nth_exn lines 1 |> Shared.extract_ints |> List.map ~f:Bigint.of_int in
+  let items = List.nth_exn lines 1 |> Shared.extract_ints in
   let op = List.nth_exn lines 2 |> parse_op |> Option.value_exn in
   let test = List.nth_exn lines 3 |> Shared.extract_ints |> List.hd_exn in
   let test_true = List.nth_exn lines 4 |> Shared.extract_ints |> List.hd_exn in
@@ -99,6 +99,12 @@ let%expect_test "Parse full input" =
 let display_monkeys = Array.map ~f:(fun (x, _, _, _, _, _) -> x)
 
 let process_monkey (divide_by_3 : bool) (monkeys : monkey array) (i : int) : monkey array =
+  let all_monkeys_lcm =
+    monkeys
+    |> Array.to_list
+    |> List.map ~f:(fun (_, _, x, _, _, _) -> x)
+    |> List.reduce_exn ~f:( * )
+  in
   let items, op, test, test_true, test_false, num_inspected = monkeys.(i) in
   let rec process_monkey' items num_inspected' =
     match items with
@@ -109,21 +115,30 @@ let process_monkey (divide_by_3 : bool) (monkeys : monkey array) (i : int) : mon
     | item :: items' ->
       let worry_level =
         match op with
-        | Plus n -> Bigint.( + ) (Bigint.of_int n) item
-        | Times n -> Bigint.( * ) (Bigint.of_int n) item
-        | PlusOld -> Bigint.( + ) item item
-        | TimesOld -> Bigint.( * ) item item
+        | Plus n -> Int.( + ) n item
+        | Times n -> Int.( * ) n item
+        | PlusOld -> Int.( + ) item item
+        | TimesOld -> Int.( * ) item item
       in
       let worry_level' =
-        if divide_by_3 then Bigint.( / ) worry_level (Bigint.of_int 3) else worry_level
+        if divide_by_3
+        then Int.( / ) worry_level 3
+        else Int.( % ) worry_level all_monkeys_lcm
+        (* else Int.( % ) worry_level 1 *)
       in
-      let worry_level_check =
-        Bigint.( = ) (Bigint.( % ) worry_level' (Bigint.of_int test)) Bigint.zero
-      in
+      let worry_level_check = Int.( = ) (Int.( % ) worry_level' test) 0 in
       let next_monkey = if worry_level_check then test_true else test_false in
       let items2, op2, test2, test_true2, test_false2, num_inspected2 =
         monkeys.(next_monkey)
       in
+      (* print_string "Processing Monkey ";
+      print_s [%sexp (i, op, test : int * operation * int)];
+      print_string " with item ";
+      print_s [%sexp (item : int)];
+      print_string "Giving item ";
+      print_s [%sexp (worry_level' : int)];
+      print_string " to Monkey ";
+      print_s [%sexp (next_monkey : int)]; *)
       monkeys.(next_monkey)
         <- ( List.append items2 [ worry_level' ]
            , op2
@@ -140,7 +155,7 @@ let%expect_test "First monkey goes" =
   print_s
     [%sexp
       (small_input |> parse |> (fun x -> process_monkey true x 0) |> display_monkeys
-        : Bigint.t list array)];
+        : int list array)];
   [%expect {|
     (() (54 65 75 74) (79 60 97) (74 500 620)) |}]
 ;;
@@ -156,7 +171,7 @@ let%expect_test "Turn of monkeys" =
   print_s
     [%sexp
       (small_input |> parse |> do_turn_of_monkeys true |> display_monkeys
-        : Bigint.t list array)];
+        : int list array)];
   [%expect {|
     ((20 23 27 26) (2080 25 167 207 401 1046) () ()) |}]
 ;;
@@ -203,17 +218,17 @@ module Part1 = struct
 end
 
 module Part2 = struct
-  let solve' input =
+  let solve' n input =
     List.fold
       ~init:(parse input)
-      ~f:(fun x _ -> do_turn_of_monkeys true x)
-      (List.range 0 10000)
+      ~f:(fun x _ -> do_turn_of_monkeys false x)
+      (List.range 0 n)
   ;;
 
   let solve input =
     let top_monkeys =
       input
-      |> solve'
+      |> solve' 10000
       |> Array.to_list
       |> List.map ~f:(fun (_, _, _, _, _, num_inspected) -> num_inspected)
       |> List.sort ~compare:Int.compare
@@ -222,25 +237,28 @@ module Part2 = struct
     List.nth_exn top_monkeys 0 * List.nth_exn top_monkeys 1
   ;;
 
-  (**
   let%expect_test "Solve small input" =
-    print_s [%sexp (solve' small_input : monkey array)];
-    [%expect
-      {|
-      (((8 12 63607185775640360 34) (Times 19) 23 2 3 54652)
-       ((20 22 26 43 30 30) (Plus 6) 19 2 0 45344) (() TimesOld 13 1 3 454)
-       (() (Plus 3) 17 0 1 54864)) |}]
+    print_s
+      [%sexp
+        (List.map
+           ~f:(fun n ->
+             solve' n small_input
+             |> Array.to_list
+             |> List.map ~f:(fun (_, _, _, _, _, num_inspected) -> num_inspected))
+           [ 1; 20; 1000 ]
+          : int list list)];
+    [%expect {|
+      ((2 4 3 6) (99 97 8 103) (5204 4792 199 5192)) |}]
   ;;
 
   let%expect_test "Solve small input" =
     print_s [%sexp (solve small_input : int)];
     [%expect {|
-      2998427328 |}]
+      2713310158 |}]
   ;;
 
   let%expect_test "Solve full input" =
     print_s [%sexp (solve full_input : int)];
-    [%expect {| 32389920780 |}]
+    [%expect {| 19457438264 |}]
   ;;
-  *)
 end
